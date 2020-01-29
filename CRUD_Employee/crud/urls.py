f0d2b9cc0ff0cc7a9e7ce7f889924c7ea2146be5
badilladrian <badilladrianch@gmail.com>
2020-01-29 13:28:45 -0600

    Making the welcome.html to be displayed

--- CRUD_Employee/crud/urls.py
+++ CRUD_Employee/crud/urls.py
@@ -4,20 +4,20 @@ from .views import *
 #Always import the views to the URL area
 
 urlpatterns = [
-    # La ruta 'leer' en donde listamos todos los registros o employees de la Base de Datos
-    path('employees/', EmployeeList.as_view(template_name = "employees/index.html"), name='list'),
+    # La ruta 'employees' en donde listamos todos los employees
+    path('list/', EmployeeList.as_view(template_name = "crud/list.html"), name='list'),
  
-    # La ruta 'detalles' en donde mostraremos una página con los detalles de un employees o registro 
-    path('employees/details/<int:pk>', EmployeeDetail.as_view(template_name = "employees/detalles.html"), name='details'),
+    # La ruta 'details' en donde mostraremos una página con los detalles de un employee
+    path('details/<int:pk>', EmployeeDetail.as_view(template_name = "crud/details.html"), name='details'),
  
-    # La ruta 'crear' en donde mostraremos un formulario para crear un nuevo employees o registro  
-    path('employees/create', EmployeeCreate.as_view(template_name = "employees/crear.html"), name='create'),
+    # La ruta 'create' en donde mostraremos un formulario para crear un nuevo employee
+    path('create/', EmployeeCreate.as_view(template_name = "crud/create.html"), name='create'),
  
-    # La ruta 'actualizar' en donde mostraremos un formulario para actualizar un employees o registro de la Base de Datos 
-    path('employees/update/<int:pk>', EmployeeUpdate.as_view(template_name = "employees/actualizar.html"), name='update'), 
+    # La ruta 'update' en donde mostraremos un formulario para actualizar un employee
+    path('update/<int:pk>', EmployeeUpdate.as_view(template_name = "crud/update.html"), name='update'), 
  
-    # La ruta 'eliminar' que usaremos para eliminar un employees o registro de la Base de Datos 
-    path('employees/delete/<int:pk>', EmployeeDelete.as_view(), name='delete'),    
+    # La ruta 'delete' que usaremos para eliminar un employee
+    path('delete/<int:pk>', EmployeeDelete.as_view(), name='delete'),    
 
 ]
 
